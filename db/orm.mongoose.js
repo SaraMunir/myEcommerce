a20d689a777eb933b7eb58a25dc151374d904a26
Sara Munir <saramunir011@gmail.com>
2021-02-17 00:25:18 -0500

    completed payment and added mens tees section

--- db/orm.mongoose.js
+++ db/orm.mongoose.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const bcrypt = require ( 'bcrypt' );
 
 // const { v4: uuidv4 } = require('uuid');
-mongoose.connect(`mongodb://localhost:27017/myEcommerceWeb`, {useNewUrlParser: true, useFindAndModify: false});
+// mongoose.connect(`mongodb://localhost:27017/myEcommerceWeb`, {useNewUrlParser: true, useFindAndModify: false});
+
+const password= '1qaz2wsx'
+const dbname= 'myEcommerceWeb'
+mongoose.connect(`mongodb+srv://saramunir011:${password}@cluster0.df4lk.mongodb.net/${dbname}?retryWrites=true&w=majority`, {useNewUrlParser: true, useFindAndModify: false});
+
 
 const db = require( './models' );
 const e = require('express');
